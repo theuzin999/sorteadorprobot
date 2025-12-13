6fb28b67024b5a459be12a8e1faa66726aefdc0a
mtheusthzin991 <henriquefelipe946@gmail.com>
2025-12-12 22:43:58 -0300

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Define o PATH para incluir o local do chromedriver
+export PATH=$PATH:/usr/bin
+
+echo "Iniciando bot com Python..."
+
+# Executa o script principal.
+python main.py
